209a19285e2b024152afded873170d6cd1d43fa2
RaphaelZH <haozhang@me.com>
2018-03-04 17:16:38 +0100

    feat(compute): information form La Fourchette

---
+++ serveur/index.js
@@ -0,0 +1,10 @@
+var michelin = require('./michelin');
+var laFourchette = require('./laFourchette');
+
+var json = michelin.get();
+if (json != undefined) {
+    for (var i = 1; i < json.restaurants.length; i++) {
+
+        laFourchette.get(json.restaurants[i]);
+    }
+}
